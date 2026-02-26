9a9ea87e9f30c9883b6701a2f043ac15143f629b
Mai Tri Thanh <maitrithanh06@gmail.com>
2026-02-26 20:16:18 +0700

    add Analytics

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/next"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+      <Analytics/>
       {children}
     </ThemeProvider>
   );
